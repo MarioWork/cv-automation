4e96ad4a2a28a3f217195307f66b49af73798847
MarioWork <mariovieira.work@hotmail.com>
2023-07-16 22:31:30 +0000

    linting configuration

--- front-end/apps-scripts/main.js
+++ front-end/apps-scripts/main.js
@@ -7,7 +7,7 @@
  * @param {object} e
  */
 function onInstall(e) {
-  onOpen(e);
+    onOpen(e);
 }
 
 /**
@@ -15,21 +15,23 @@ function onInstall(e) {
  * @param {object} e
  */
 function onOpen(e) {
-  var ui = DocumentApp.getUi();
+    var ui = DocumentApp.getUi();
 
-  ui.createMenu("CV Automation")
-    .addItem("Upload", "showUploadFileDialog")
-    .addSeparator()
-    .addItem("Enhance", "enhanceCVSidebar")
-    .addToUi();
+    ui.createMenu('CV Automation')
+        .addItem('Upload', 'showUploadFileDialog')
+        .addSeparator()
+        .addItem('Enhance', 'enhanceCVSidebar')
+        .addToUi();
 }
 
 function showUploadFileDialog() {
-  const html = HtmlService.createHtmlOutputFromFile("client/file-upload-dialog")
-    .setWidth(400)
-    .setHeight(300);
+    const html = HtmlService.createHtmlOutputFromFile(
+        'client/file-upload-dialog'
+    )
+        .setWidth(400)
+        .setHeight(300);
 
-  DocumentApp.getUi().showModalDialog(html, "Upload the CV");
+    DocumentApp.getUi().showModalDialog(html, 'Upload the CV');
 }
 
 function enhanceCVSidebar() {}
