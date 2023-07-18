15777dc6ad7bdec140aa9b6512e0239b4da7489d
MarioWork <mariovieira.work@hotmail.com>
2023-07-18 22:29:00 +0000

    remove unecessary file

--- back-end/prepare-env.sh
+++
@@ -1,9 +0,0 @@
-#Create Cloud Function
-gcloud functions deploy cv-automation \
-    --trigger-http \
-    --runtime=nodejs18 \
-    --entry-point=app \
-    --service-account=TODO \
-    --region=$LOCATION \
-    --no.allow-unauthenticated \
-    --update-env-vars API_KEY=YOUR_API_KEY .
