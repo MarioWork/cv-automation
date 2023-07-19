6183a4340f3dad0f292d93f3144511702ccf7aab
MarioWork <mariovieira.work@hotmail.com>
2023-07-19 14:21:25 +0000

    raw endpoint code to extract text from document

--- back-end/index.js
+++ back-end/index.js
@@ -1,9 +1,43 @@
+require('dotenv').config();
+
 const express = require('express');
+
+const { DocumentProcessorServiceClient } =
+    require('@google-cloud/documentai').v1;
+
+const fs = require('fs').promises;
+
+const filePath = 'CV-Mario-Vieira.pdf';
+
+const name = `projects/${process.env.PROJECT_NR}/locations/eu/processors/${process.env.PROCESSOR_ID}`;
 
 const app = express();
 
-app.get('/', (req, res) =>
-    res.send({ message: 'Hello World from my function!' })
-);
+const client = new DocumentProcessorServiceClient({
+    apiEndpoint: 'eu-documentai.googleapis.com'
+});
+
+app.get('/', async (req, res) => {
+    //const file = await fs.readFile(filePath);
+
+    //const encodedFile = Buffer.from(file).toString('base64');
+
+    const request = {
+        name,
+        rawDocument: {
+            content: req.body.file64encoded,
+            mimeType: 'application/pdf'
+        }
+    };
+    try {
+        const [result] = await client.processDocument(request);
+        const { document } = result;
+
+        res.send({ text: document.text });
+    } catch (error) {
+        console.log(error.message);
+        res.sendStatus(500);
+    }
+});
 
 exports.app = app;
