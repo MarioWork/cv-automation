d122b8fbd90467801b2167f775b889afc7121c3f
MarioWork <mariovieira.work@hotmail.com>
2023-07-23 01:59:24 +0000

    create document route

--- backend/src/routes/document.js
+++ backend/src/routes/document.js
@@ -1,4 +1,24 @@
 const express = require('express');
+
+const { to } = require('await-to-js');
+
+const { extractText } = require('path');
+
 const router = express.Router();
 
-router.post('/', () => {});
+router.post('/', async (req, res) => {
+    const file = req.body?.file;
+
+    if (!file) return res.status(400).send('64BaseEncoded file missing.');
+
+    const [error, response] = await to(extractText(file));
+
+    if (error) {
+        console.log(error);
+        return res.sendStatus(500);
+    }
+
+    const { document } = response;
+
+    return res.send({ text: document.text });
+});
