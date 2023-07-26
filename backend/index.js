414b65ede19afd4513590882d30b2fbd0b2ef7a4
MarioWork <mariovieira.work@hotmail.com>
2023-07-26 11:16:00 +0000

    Use helmet middleware for better security

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,9 @@
 const express = require('express');
+const helmet = require('helmet');
 
 const app = express();
+
+app.use(helmet());
 
 app.use('/document', require('./src/routes/document'));
 app.use('/chat', require('./src/routes/chat'));
