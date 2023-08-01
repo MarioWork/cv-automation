0264f2a9798139b28e5e94b6212f8a38ae522293
MarioWork <mariovieira.work@hotmail.com>
2023-08-01 14:14:12 +0000

    v2 vertex ai create data structure function

--- backend/src/services/vertex-ai.js
+++ backend/src/services/vertex-ai.js
@@ -1,13 +1,39 @@
-const { aiplatform } = require('@google-cloud/aiplatform');
+const aiplatform = require('@google-cloud/aiplatform');
 
-const client = new aiplatform.ModelServiceClient();
+const { PredictionServiceClient } = aiplatform.v1;
 
-exports.organizeDataIntoDataStructure = ({ data }) => {
+const { helpers } = aiplatform;
+
+const predictionServiceClient = new PredictionServiceClient();
+
+module.exports = async function callPredict() {
+    const prompt = {
+        prompt: 'Give me ten interview questions for the role of program manager.'
+    };
+    const instanceValue = helpers.toValue(prompt);
+    const instances = [instanceValue];
+
+    console.log(instances[0].structValue.fields);
+
+    const parameter = {
+        temperature: 0.2,
+        maxOutputTokens: 5,
+        topP: 0.95,
+        topK: 40
+    };
+    const parameters = helpers.toValue(parameter);
+
     const request = {
-        projectId: process.env.GC_PROJECT_ID,
-        modelId: process.env.GC_TEXT_MODEL_ID,
-        text: data
+        endpoint: process.env.VERTEX_AI_TEXT_ENDPOINT,
+        instances,
+        parameters
     };
 
-    return client.predict(request);
+    try {
+        const response = await predictionServiceClient.predict(request);
+        console.log('Get text prompt response');
+        console.log(response);
+    } catch (error) {
+        console.log(error);
+    }
 };
