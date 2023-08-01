194b5fb236efd479d2d6a3a496ab73fdee566b07
MarioWork <mariovieira.work@hotmail.com>
2023-08-01 14:13:56 +0000

    extract code to controller

--- backend/src/routes/document.js
+++ backend/src/routes/document.js
@@ -1,26 +1,7 @@
 const express = require('express');
 
-const { to } = require('await-to-js');
-
-const { extractText } = require('../services/document-ai');
-
 const router = express.Router();
 
-router.post('/', async (req, res) => {
-    const file = req.body?.file;
-
-    if (!file) return res.status(400).send('64BaseEncoded file missing.');
-
-    const [error, response] = await to(extractText({ file }));
-
-    if (error) {
-        console.log(error);
-        return res.sendStatus(500);
-    }
-
-    const { document } = response[0];
-
-    return res.send(document.text);
-});
+router.post('/', require('../controllers/document-controller'));
 
 module.exports = router;
