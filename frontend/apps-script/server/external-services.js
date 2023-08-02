7e68b834bd7588872b28ca9d0a6bdcc04f6943ca
MarioWork <mariovieira.work@hotmail.com>
2023-08-02 13:37:12 +0000

    first iteration of request to get data from document

--- frontend/apps-script/server/external-services.js
+++ frontend/apps-script/server/external-services.js
@@ -1,3 +1,17 @@
 const extractText = async data => {
-    return data.data;
+    const accessToken = ScriptApp.getOAuthToken();
+
+    const config = {
+        method: 'POST',
+        headers: {
+            Authorization: 'Bearer ' + accessToken
+        }
+    };
+
+    const response = UrlFetchApp.fetch(
+        'https://europe-west2-cv-automation-391816.cloudfunctions.net/cv-automation-api/document',
+        config
+    );
+
+    Logger.log(response);
 };
