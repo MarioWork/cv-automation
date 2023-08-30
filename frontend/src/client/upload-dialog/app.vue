8da1de56feb877ee8934ecf33c86659dde833853
MarioWork <mariovieira.work@hotmail.com>
2023-08-30 20:08:21 +0000

    frontend: file upload dialog v2

--- frontend/src/client/upload-dialog/app.vue
+++ frontend/src/client/upload-dialog/app.vue
@@ -1,16 +1,57 @@
 <template>
-    <button @click="openFileExplorer">Choose File</button>
-    <input name="file" type="file" style="display: none" ref="fileInput" />
+    <main
+        style="
+            width: 100vw;
+            height: 100vh;
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            gap: 10px;
+        "
+    >
+        <button
+            style="
+                width: 200px;
+                height: 200px;
+                background-color: transparent;
+                border-radius: 20px;
+                border-style: dashed;
+                border-color: red;
+                cursor: pointer;
+            "
+            @click="openFileExplorer"
+        >
+            Choose File
+        </button>
+
+        <button
+            style="
+                width: 200px;
+                background-color: red;
+                color: white;
+                padding: 15px;
+                border: none;
+                border-radius: 20px;
+                cursor: pointer;
+            "
+        >
+            Process
+        </button>
+
+        <input name="file" style="display: none" type="file" ref="fileInput" />
+    </main>
 </template>
 
 <script setup>
-    /*  google.script.run
-        .withSuccessHandler(message => console.log(message))
-        .withFailureHandler(err => console.log(err.message))
-        .test(); */
     import { ref } from 'vue';
 
     const fileInput = ref(null);
 
     const openFileExplorer = () => fileInput.value.click();
+
+    const processCv = () =>
+        google.script.run
+            .withSuccessHandler(message => console.log(message))
+            .withFailureHandler(err => console.log(err.message))
+            .test();
 </script>
