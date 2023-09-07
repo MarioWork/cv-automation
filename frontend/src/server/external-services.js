45fd2609cfbc6660084afb1df25099b008244e05
MarioWork <mariovieira.work@hotmail.com>
2023-09-07 11:29:10 +0000

    frontend: remove comments

--- frontend/src/server/external-services.js
+++ frontend/src/server/external-services.js
@@ -21,31 +21,5 @@ const processCv = async ({ base64File }) => {
 
     const docBody = DocumentApp.getActiveDocument().getBody();
 
-    //Object.keys(data).forEach(key => docBody.appendParagraph(data[key]));
     docBody.appendParagraph(JSON.stringify(data));
 };
-
-/*const displayDataOnDoc = data => {
-    const docId = DocumentApp.getActiveDocument().getId();
-
-    Object.keys(data).forEach(key => {
-        const body = doc.getBody();
-
-        body.appendParagraph(key);
-
-        const dataValue = data[key];
-
-        if (Array.isArray(dataValue)) {
-            dataValue.forEach(value => displayDataOnDoc(value));
-            return;
-        }
-
-        if (typeof dataValue === 'object') {
-            displayDataOnDoc(dataValue);
-            return;
-        }
-
-        body.appendParagraph(dataValue);
-        doc.saveAndClose();
-    });
-};*/
