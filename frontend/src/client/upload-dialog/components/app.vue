106e87043cadb064e3d5230fd4d177a5a6ef1de9
MarioWork <mariovieira.work@hotmail.com>
2023-09-08 22:44:18 +0100

    refactor(frontend): :sparkles: remove code that was moved to composable && use new composable state and methods

--- frontend/src/client/upload-dialog/components/app.vue
+++ frontend/src/client/upload-dialog/components/app.vue
@@ -36,7 +36,7 @@
 
         <button
             class="process-button"
-            @click.prevent="createBase64File"
+            @click.prevent="process"
             :disabled="isLoading"
         >
             <div v-if="isLoading" class="loader"></div>
@@ -64,18 +64,24 @@
 </template>
 
 <script setup>
+    import { ref } from 'vue';
+
     import OskonLogo from '../../common/components/oskon-logo';
-    import { ref, watch } from 'vue';
+
     import { useDocument } from '../composables/use-document';
 
-    const { file, error, insertFile, clearFile, clearError, setError } =
-        useDocument();
+    const {
+        file,
+        error,
+        isLoading,
+        insertFile,
+        clearFile,
+        clearError,
+        process
+    } = useDocument();
 
-    //TODO: Separate code into separate components or composable
     const fileInput = ref(null);
     const isDragging = ref(false);
-    const isLoading = ref(false);
-    const base64File = ref(null);
 
     const onRemoveFileClick = () => clearFile();
 
@@ -84,9 +90,7 @@
         e.dataTransfer.dropeffect = 'copy';
     };
 
-    const onDragLeave = () => {
-        isDragging.value = false;
-    };
+    const onDragLeave = () => (isDragging.value = false);
 
     const onDrop = e => {
         isDragging.value = false;
@@ -96,48 +100,4 @@
     const onSelectFileClick = () => fileInput.value.click();
 
     const onFileChanged = event => insertFile(event.target.files[0]);
-
-    const createBase64File = () => {
-        isLoading.value = true;
-
-        if (!file.value) {
-            setError(new Error('No file selected'));
-            isLoading.value = false;
-            return;
-        }
-
-        console.log(file.value);
-
-        isLoading.value = true;
-
-        const reader = new FileReader();
-        reader.onload = () => (base64File.value = reader.result.split(',')[1]);
-        reader.readAsDataURL(file.value);
-
-        clearError();
-    };
-
-    const processCv = async () => {
-        if (!base64File.value) {
-            setError(new Error('No file selected'));
-            isLoading.value = false;
-            return;
-        }
-
-        clearError();
-
-        google.script.run
-            .withSuccessHandler(() => {
-                isLoading.value = false;
-                clearError();
-            })
-            .withFailureHandler(err => {
-                isLoading.value = false;
-                setError(err);
-                console.log(err.message);
-            })
-            .processCv({ base64File: base64File.value });
-    };
-
-    watch(base64File, processCv);
 </script>
