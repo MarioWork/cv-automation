2239cea4694277501e7a450fad0a5cfd72f2a39d
MarioWork <mariovieira.work@hotmail.com>
2023-09-08 22:43:44 +0100

    refactor(frontend): :sparkles: reorganized code into composable instead of the view

--- frontend/src/client/upload-dialog/composables/use-document.js
+++ frontend/src/client/upload-dialog/composables/use-document.js
@@ -1,17 +1,51 @@
 import { ref } from 'vue';
 
+const errorType = {
+    NO_FILE: new Error('No such file selected'),
+    UNKNOWN: new Error('Oops! Something went wrong')
+};
+
+const createBase64File = file =>
+    new Promise((resolve, reject) => {
+        const reader = new FileReader();
+        reader.onload = () => resolve(reader.result.split(',')[1]);
+        reader.onerror = reject(reader.error);
+        reader.readAsDataURL(file.value);
+    });
+
 exports.useDocument = () => {
     const state = {
         file: ref(null),
-        error: ref(null)
+        error: ref(null),
+        isLoading: ref(false)
+    };
+
+    const process = async () => {
+        if (!state.file.value) {
+            state.error.value = errorType.NO_FILE;
+            state.isLoading.value = false;
+            return;
+        }
+
+        state.isLoading.value = true;
+
+        try {
+            const base64File = await createBase64File(state.file.value);
+            google.script.run.processCv({ base64File });
+        } catch (error) {
+            state.error.value = errorType.UNKNOWN;
+        }
+
+        state.isLoading.value = false;
     };
 
     return {
         file: state.file,
         error: state.error,
-        setError: error => (state.error.value = error),
+        isLoading: state.isLoading,
         insertFile: file => (state.file.value = file),
         clearFile: () => (state.file.value = null),
-        clearError: () => (state.error.value = null)
+        clearError: () => (state.error.value = null),
+        process
     };
 };
