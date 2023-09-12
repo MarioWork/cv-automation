51b4c488c8c7a4a4818b4b44238eeb37078f3aed
MarioWork <mariovieira.work@hotmail.com>
2023-09-12 20:28:40 +0000

    feat(frontend): :building_construction: working on developing tempalte with retrieved data

--- frontend/src/server/document-service.js
+++ frontend/src/server/document-service.js
@@ -13,13 +13,28 @@ const processCv = async ({ base64File }) => {
         }
     };
 
-    const response = UrlFetchApp.fetch(
+    /*  const response = UrlFetchApp.fetch(
         'https://europe-west2-cv-automation-391816.cloudfunctions.net/cv-automation-api/document',
         config
     );
     const data = JSON.parse(response.getContentText('UTF-8')).data;
-
+ */
     const docBody = DocumentApp.getActiveDocument().getBody();
 
-    docBody.appendParagraph(JSON.stringify(data));
+    /*     docBody.appendParagraph(JSON.stringify(data));
+     */
+
+    const image = UrlFetchApp.fetch(
+        'https://storage.googleapis.com/cv-document-images/oskon-logo.png'
+    ).getBlob();
+
+    const docImage = docBody.insertImage(0, image);
+
+    docImage.setAttributes({
+        [DocumentApp.Attribute.HORIZONTAL_ALIGNMENT]:
+            DocumentApp.HorizontalAlignment.RIGHT
+    });
+
+    docImage.setWidth(250);
+    docImage.setHeigh(150);
 };
