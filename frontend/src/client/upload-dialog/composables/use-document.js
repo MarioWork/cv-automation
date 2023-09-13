22d18ed15163a5d07540602ed53fdf592215fd26
MarioWork <mariovieira.work@hotmail.com>
2023-09-13 16:02:09 +0000

    feat(frontend): :construction_worker: added a new state to keep up with the current process executing

--- frontend/src/client/upload-dialog/composables/use-document.js
+++ frontend/src/client/upload-dialog/composables/use-document.js
@@ -8,11 +8,20 @@ const errorType = {
     UNKNOWN: new Error('Oops! Something went wrong')
 };
 
+const processType = {
+    CLEAN_UP: 'Cleaning up',
+    EXTRACTING: 'Extracting data',
+    WRITING: 'Writing data',
+    EMPTY: ''
+};
+
+//TODO: add a label to button saying extracting and writting
 exports.useDocument = () => {
     const state = {
         file: ref(null),
         error: ref(null),
-        isLoading: ref(false)
+        isLoading: ref(false),
+        currentProcess: ref(processType.EMPTY)
     };
 
     const process = async () => {
@@ -24,19 +33,32 @@ exports.useDocument = () => {
 
         state.error.value = null;
         state.isLoading.value = true;
-
+        //TODO: server functions names be a enum
         try {
+            state.currentProcess.value = processType.CLEAN_UP;
+            await runServerFunction({ functionName: 'clearDocument' });
+
+            state.currentProcess.value = processType.EXTRACTING;
             const base64File = await createBase64File(state.file.value);
-            await runServerFunction({
-                functionName: 'processCv',
+            const data = await runServerFunction({
+                functionName: 'processDocument',
                 data: { base64File }
             });
+
+            if (!data) throw Error();
+
+            state.currentProcess.value = processType.WRITING;
+            await runServerFunction({
+                functionName: 'writeDataToDocument',
+                data: data
+            });
         } catch (error) {
-            console.log(error);
-            state.error.value = error.message;
-            /*             state.error.value = errorType.UNKNOWN; */
+            state.currentProcess.value = processType.EMPTY;
+            state.error.value = errorType.UNKNOWN;
         }
 
+        google.script.host.close();
+        state.currentProcess.value = processType.EMPTY;
         state.isLoading.value = false;
     };
 
@@ -44,6 +66,7 @@ exports.useDocument = () => {
         file: state.file,
         error: state.error,
         isLoading: state.isLoading,
+        currentProcess: state.currentProcess,
         insertFile: file => (state.file.value = file),
         clearFile: () => (state.file.value = null),
         clearError: () => (state.error.value = null),
