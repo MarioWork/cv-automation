02307362453c81a8d0ce9913c56b41edee4f8c3d
MarioWork <mariovieira.work@hotmail.com>
2023-09-13 14:44:49 +0000

    refactor(backend(document)): :construction_worker: return as javascript object instead of string

--- backend/src/services/vertex-ai.js
+++ backend/src/services/vertex-ai.js
@@ -38,6 +38,5 @@ exports.organizeDataIntoDataStructure = async ({ promptData }) => {
         response[0].predictions[0].structValue.fields.candidates.listValue
             .values[0].structValue.fields.content.stringValue;
 
-    //TODO: Return this as an object instead of string
-    return data;
+    return JSON.parse(data);
 };
