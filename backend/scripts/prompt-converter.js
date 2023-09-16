5e37b0cabdb9ba5bf702d80890d7fe7ff50f660b
MarioWork <mariovieira.work@hotmail.com>
2023-09-16 02:07:14 +0000

    feat(backend(document)): :sparkles: created a script to convert the exported prompt from vertex ai to javascript function that creates prompt

---
+++ backend/scripts/prompt-converter.js
@@ -0,0 +1,28 @@
+const fs = require('fs');
+
+const destDir = './src/utils/';
+const fileName = 'prompt-creator.js';
+
+const path = './scripts/test.json';
+
+const { context, examples } = JSON.parse(fs.readFileSync(path, 'utf8'));
+
+const transformedExamples = examples.map(ex => ({
+    ...ex,
+    output: {
+        ...ex.output,
+        content: ex.output.content.replace(/(".*?"|[^"\s]+)\s*/g, '$1')
+    }
+}));
+
+//$$$data$$$ so we can replace it with just data without " " and $$$ so it behaves as a variable with a value instead of a string on the written file
+const prompt = JSON.stringify({
+    context,
+    examples: transformedExamples,
+    messages: [{ author: 'user', content: '$$$data$$$' }]
+}).replace('"$$$data$$$"', 'data');
+
+//Create prompt creation
+fs.writeFileSync(destDir + fileName, `module.exports = data => (${prompt})`);
+
+console.log('File created at: ' + destDir + fileName);
