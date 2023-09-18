d355c77409f23174dc488d0e370c1b4408ed8120
MarioWork <mariovieira.work@hotmail.com>
2023-09-18 16:00:05 +0000

    feat(backend(chat)): :sparkles: use new middleware

--- backend/index.js
+++ backend/index.js
@@ -3,9 +3,11 @@ const helmet = require('helmet');
 
 const app = express();
 
-app.use(helmet());
-
 app.use('/document', require('./src/routes/document'));
 app.use('/chat', require('./src/routes/chat'));
+
+app.use(helmet());
+
+app.use(require('./src/middleware/error-handler'));
 
 exports.app = app;
