49dcaf713867a5c28982f2179e68c38f249a06cf
MarioWork <mariovieira.work@hotmail.com>
2023-09-18 16:12:49 +0000

    refactor(backend(document)): :building_construction: refactored document controller to be minimal using the new error handler

--- backend/src/controllers/document-controller.js
+++ backend/src/controllers/document-controller.js
@@ -1,36 +1,16 @@
+const createError = require('http-errors');
+
+const { errorMessages } = require('../utils/error-messages');
 const { extractText } = require('../services/document-ai');
 const { organizeDataIntoDataStructure } = require('../services/vertex-ai');
-const { to } = require('await-to-js');
 
-module.exports = async (req, res) => {
+module.exports = async (req, res, next) => {
     const file = req.body?.file;
 
-    if (!file) return res.status(400).send('64BaseEncoded file missing.');
+    if (!file) next(new createError(400, errorMessages.MISSING_FILE_FIELD));
 
-    console.log('Extracting text...');
-
-    const [docError, docText] = await to(extractText({ file }));
-
-    console.log('Finished Extracting text...');
-
-    //TODO: MOve errors to service
-    if (docError) {
-        console.log(docError);
-        return res.sendStatus(500);
-    }
-
-    console.log('Creating data structure...');
-
-    const [aiError, data] = await to(
-        organizeDataIntoDataStructure({ promptData: docText })
-    );
-
-    console.log('Finished Creating data structure...');
-
-    //TODO: MOve errors to service
-    if (aiError) {
-        console.log(aiError);
-        return res.sendStatus(500);
-    }
-    return res.send(data);
+    extractText({ file })
+        .then(organizeDataIntoDataStructure)
+        .then(data => res.send(data))
+        .catch(error => next(error));
 };
