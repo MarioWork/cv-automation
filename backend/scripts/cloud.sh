60ffd5e4d399ac74ec65d31c1e9108fa8a441a8e
MarioWork <mariovieira.work@hotmail.com>
2023-10-04 13:14:07 +0000

    refactor(backend(document)): :construction_worker: refactor the deployment script to be 100%

--- backend/scripts/cloud.sh
+++ backend/scripts/cloud.sh
@@ -1,4 +1,6 @@
-export PROJECT_ID=$(gcloud config list --format 'value(core.project)')
+export PROJECT_ID=$(gcloud config list --format 'value(core.project)');
+export PROJECT_NR=$(gcloud projects describe $PROJECT_ID --format='value(projectNumber)');
+export LOCATION="us-central1";
 
 gcloud functions deploy cv-automation-api \
     --gen2 \
@@ -8,4 +10,5 @@ gcloud functions deploy cv-automation-api \
     --service-account=doc-vertex-ai-cloud-function@$PROJECT_ID.iam.gserviceaccount.com \
     --region=europe-west2 \
     --no-allow-unauthenticated \
-    --memory=512
+    --memory=512 \
+    --set-env-vars VERTEX_AI_TEXT_ENDPOINT="projects/$PROJECT_ID/locations/$LOCATION/publishers/google/models/chat-bison",DOC_AI_PROCESSOR_ENDPOINT="projects/$PROJECT_NR/locations/eu/processors/b910bbfdea5937b4",GC_PROJECT_ID=$PROJECT_ID
