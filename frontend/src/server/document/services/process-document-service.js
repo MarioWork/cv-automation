d863931caae14d094ac841c30ac090fb30f5a063
MarioWork <mariovieira.work@hotmail.com>
2023-10-14 22:32:49 +0000

    chore(frontend): :memo: doc document extraction server function

--- frontend/src/server/document/services/process-document-service.js
+++ frontend/src/server/document/services/process-document-service.js
@@ -1,3 +1,8 @@
+/**
+ * Receives the user google identity token and the 64String File and returns extracted text
+ * @param {{idToken: string, base64File: string}} params
+ * @returns {{data: string}} extracted text
+ */
 const processDocumentService_ = ({ idToken, base64File }) => {
     const config = {
         method: 'POST',
