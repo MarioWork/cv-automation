2d3678a1c9bbbb6f259ef97d74e71a283177921e
MarioWork <mariovieira.work@hotmail.com>
2023-10-14 22:20:31 +0000

    chore(frontend): :memo: docs

--- frontend/src/client/common/utils/create-base-64-file.js
+++ frontend/src/client/common/utils/create-base-64-file.js
@@ -1,5 +1,10 @@
+/**
+ * Returns a promise that when resolved returns the 64 string encoded file
+ * @param {File} file
+ * @returns {Promise}
+ */
 exports.createBase64File = file =>
-    new Promise((resolve, reject) => {
+    new Promise((resolve, _) => {
         const reader = new FileReader();
         reader.onload = () => resolve(reader.result.split(',')[1]);
         reader.readAsDataURL(file);
--- frontend/src/client/common/utils/run-server-function.js
+++ frontend/src/client/common/utils/run-server-function.js
@@ -1,3 +1,8 @@
+/**
+ *  Executes a google apps script server function by name and return promise
+ * @param {{functionName: string, data: *}} params
+ * @returns
+ */
 exports.runServerFunction = ({ functionName, data }) =>
     new Promise((resolve, reject) => {
         google.script.run
